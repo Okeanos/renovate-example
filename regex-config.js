73528424e8c2a5d0d025870c72b2d19ee680a22e
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2022-12-03 16:37:39 +0100

    fix missing sourceURL in regex action

--- regex-config.js
+++ regex-config.js
@@ -8,7 +8,7 @@ module.exports = {
 				"(?<depName>go\\.nikolasgrottendieck\\.com\\/[\\/a-zA-Z0-9\\.-]*?) (?<currentValue>[a-zA-Z0-9\\.-]+)(.*)\\n"
 			],
 			"datasource": "go",
-			"sourceUrl": "",
+			"sourceUrl": "{{{replace 'go.nikolasgrottendieck.com' 'https://okeanos-azure@dev.azure.com/okeanos-azure/renovate%20me/_git/renovate%20me/_git/' sourceUrl}}}",
 			"depNameTemplate:": "{{{replace 'go.nikolasgrottendieck.com' 'https://okeanos-azure@dev.azure.com/okeanos-azure/renovate%20me/_git/renovate%20me/_git/' depName}}}"
 		}
 	]
