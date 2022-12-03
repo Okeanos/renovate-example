63bbe097df91010a1468ed64f18a17ac7c98f425
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2022-12-03 17:04:30 +0100

    attempt to fix regex pipeline

--- regex-config.js
+++ regex-config.js
@@ -5,11 +5,10 @@ module.exports = {
 				"^go.mod$"
 			],
 			"matchStrings": [
-				"(?<depName>go\.nikolasgrottendieck\.com\/[\/a-zA-Z0-9\.-]+) (?<currentValue>[a-zA-Z0-9\.-]+)(.*)\\n"
+				"go\.nikolasgrottendieck\.com\/(?<packageName>[\/a-zA-Z0-9\.-]+) (?<currentValue>[a-zA-Z0-9\.-]+)(.*)\\n"
 			],
-			"datasource": "go",
-			"sourceUrl": "{{{replace 'go.nikolasgrottendieck.com' 'https://okeanos-azure@dev.azure.com/okeanos-azure/renovate%20me/_git/renovate%20me/_git/' sourceUrl}}}",
-			"depNameTemplate:": "{{{replace 'go.nikolasgrottendieck.com' 'https://okeanos-azure@dev.azure.com/okeanos-azure/renovate%20me/_git/renovate%20me/_git/' depName}}}"
+			"datasourceTemplate": "go",
+			"depNameTemplate:": "https://okeanos-azure@dev.azure.com/okeanos-azure/renovate%20me/_git/renovate%20me/_git/{{{packageName}}}"
 		}
 	]
 };
