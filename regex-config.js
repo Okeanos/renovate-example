513c1f56cf26b9ecd83329290673f3a951304ccb
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2022-12-03 16:34:29 +0100

    fix renovate config usage in regex pipeline

---
+++ regex-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	"regexManagers": [
+		{
+			"fileMatch": [
+				"^go.mod$"
+			],
+			"matchStrings": [
+				"(?<depName>go\\.nikolasgrottendieck\\.com\\/[\\/a-zA-Z0-9\\.-]*?) (?<currentValue>[a-zA-Z0-9\\.-]+)(.*)\\n"
+			],
+			"datasource": "go",
+			"sourceUrl": "",
+			"depNameTemplate:": "{{{replace 'go.nikolasgrottendieck.com' 'https://okeanos-azure@dev.azure.com/okeanos-azure/renovate%20me/_git/renovate%20me/_git/' depName}}}"
+		}
+	]
+};
